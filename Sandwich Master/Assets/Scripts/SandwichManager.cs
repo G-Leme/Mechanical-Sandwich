46772e925569c7276c2edbe1db03d6a15297a0b3
Gustavo Leme de Oliveira <gustavooleme.oliveira@gmail.com>
2023-06-15 17:47:40 -0300

    Finishing main mechanics

---
+++ Sandwich Master/Assets/Scripts/SandwichManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SandwichManager : MonoBehaviour
+{
+    [SerializeField] private Sandwich currentSandwich;
+    [SerializeField] private Sandwich[] sandwiches;
+
+    public List<GameObject> selectedIngredients = new List<GameObject>();
+    public List<GameObject> correctIngredients = new List<GameObject>();
+
+    public int score;
+    public TextMeshProUGUI scoreboard;
+
+    public TextMeshProUGUI sandwichName;
+    public Image sandwichIconDisplay;
+    public Image sandwichRecipeDisplay;
+
+    public bool sandwichIsDone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSandwich = sandwiches[Random.Range(0, sandwiches.Length)];   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreboard.text = ("Score: " + score.ToString()); 
+
+        sandwichName.text = currentSandwich.sandwichName;
+
+        correctIngredients = currentSandwich.ingredients;
+
+        sandwichIconDisplay.sprite = currentSandwich.sandwichIcon;
+
+        sandwichRecipeDisplay.sprite = currentSandwich.sandwichRecipe;
+
+        if (sandwichIsDone == true)
+        {
+            
+            currentSandwich = sandwiches[Random.Range(0, sandwiches.Length)];
+            sandwichIsDone = false;
+        }
+
+      CompareLists();
+      
+    }
+
+    private void CompareLists()
+    {
+        if (selectedIngredients.Count != correctIngredients.Count)
+        {
+            return;
+        }
+        else
+        {
+                if (correctIngredients[0] == selectedIngredients[0] && correctIngredients[1] == selectedIngredients[1]
+                   && correctIngredients[2] == selectedIngredients[2])
+                {
+                    sandwichIsDone = true;
+                    selectedIngredients.Clear();
+                    score += 50;
+                }
+                else
+                {
+                    sandwichIsDone = true;
+                    selectedIngredients.Clear();
+                if(score > 0)
+                    score -= 50;
+                }
+            
+
+        }
+    }
+
+
+}
--- Sandwich Master/Assets/Scripts/SelectIngredients.cs
+++ Sandwich Master/Assets/Scripts/SelectIngredients.cs
@@ -7,8 +7,31 @@ public class SelectIngredients : MonoBehaviour
 
     [SerializeField] private GameObject ingredient;
     [SerializeField] private Transform ingredientPos;
-    
+    [SerializeField] private SandwichManager sandwichManagerScript;
+    [SerializeField] private GameObject sandwichManager;
+  
 
+
+    private bool canClick;
+
+    private void Start()
+    {
+        canClick = true;
+        sandwichManagerScript = sandwichManager.GetComponent<SandwichManager>();
+    }
+
+    private void Update()
+    {
+        if (sandwichManagerScript.sandwichIsDone == true)
+        {
+            DestroyInstances();
+            canClick = true;
+        }
+
+      
+        
+
+    }
     private Vector3 MouseWorldPosition()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -16,6 +39,22 @@ public class SelectIngredients : MonoBehaviour
 
     private void OnMouseDown()
     {
-        var ingredientInstance = GameObject.Instantiate(ingredient, ingredientPos.position, Quaternion.Euler(new Vector3(0,0, 49)));
+        if (canClick == true)
+        {
+           
+            var ingredientInstance = GameObject.Instantiate(ingredient, ingredientPos.position, Quaternion.Euler(new Vector3(0, 0, 49)));
+            sandwichManagerScript.selectedIngredients.Add(ingredient);
+            canClick = false;
+           
+        }
+    }
+
+    private void DestroyInstances()
+    {
+        GameObject[] ingredientInstances = GameObject.FindGameObjectsWithTag("Ingredients");
+        foreach(GameObject instance in ingredientInstances)
+        {
+            GameObject.Destroy(instance, 1f);
+        }
     }
 }
